24ba5a7bed5282d6819559153d0548dc48b01101
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-07-19 22:08:24 -0400

    why can i not see the .gitignore file? this should be fine

---
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -0,0 +1,6 @@
+class PokemonsController < ApplicationController
+	def destroy
+		byebug
+		# pokemon = Pokemon.find_by(id: )
+	end
+end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,56 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", sitesetUp)
+
+function sitesetUp() {
+	fetch(TRAINERS_URL)
+		.then((response) => response.json())
+		.then((jso) => {setUpTrainersandPokemons(jso)});
+
+}
+
+function setUpTrainersandPokemons(trainerArr) {
+	let main = document.querySelector("main")
+
+	trainerArr.forEach((trainer) => {
+		let div = document.createElement("div")
+		div.className = "card"
+		div["data-id"] = trainer.id
+		div.innerHTML = `<p>${trainer.name}</p>
+  						 <button data-trainer-id="${trainer.id}">Add Pokemon</button>`
+
+  		let ul = document.createElement("ul")
+  		trainer.pokemons.forEach((pokemon) => {
+  				let li = document.createElement("li")
+  				li.innerHTML = `${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>`
+  				ul.appendChild(li)
+  			}
+  		)
+  		div.appendChild(ul)
+  		main.appendChild(div)
+		}
+	)
+
+	trainerArr.forEach((trainer) => {
+		trainer.pokemons.forEach((pokemon) => {
+			let pokemonDeleteButton = document.querySelector(`[data-pokemon-id='${pokemon.id}']`)
+			pokemonDeleteButton.addEventListener("click", pokeRelease)
+			}
+		)
+		}
+	)
+}
+
+function pokeRelease(event) {
+	let pokeId = event.target.dataset.pokemonId
+	let deleteConfig = {
+        method: 'DELETE'
+    }
+    
+    fetch(POKEMONS_URL + `/${pokeId}`, deleteConfig)
+    	.then((response) => console.log(response))
+
+
+}
